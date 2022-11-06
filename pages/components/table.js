f38e9e6c38b41808b985c249fa24c6ee59dca3a7
evribsap <evribsaputra@gmail.com>
2022-11-06 15:48:51 +0700

    add redux

--- pages/components/table.js
+++ pages/components/table.js
@@ -1,11 +1,14 @@
 //import data from '/database/data.json';
 import { BiEdit, BiTrashAlt} from 'react-icons/bi';
-import { getUser } from '../../lib/helper';
+import { getUsers } from '../../lib/helper';
 import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
 
 export default function Table () {
 
-    const { isLoading, isError, data, error }= useQuery('users', getUser)
+    const state = useSelector((state) => state)
+    console.log(state)
+    const { isLoading, isError, data, error }= useQuery('users', getUsers)
 
     if(isLoading) {
         return (
